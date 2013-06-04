b3ffbe3076b628d3bd18102caa1d5ca12ca603ab
Mike Walker <noisufnoc@gmail.com>
2013-06-03 21:41:22 -0700

    params and teapots

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
-from flask import Flask
+from flask import Flask, abort, request
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
+
+@app.route('/params', methods=['GET'])
+def params():
+    data = request.args.get('data', None)
+    if data is None:
+        abort(418)
+    else:
+        return 'My data is %s' % data
 
 if __name__ == '__main__':
     app.debug = True
